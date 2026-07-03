9db374fc8d31c9b271b865bb5ef48dcad80f779c
NazrulShafiq <nazrulshafiq498@gmail.com>
2026-07-02 18:45:10 -0700

    #1: Add new table to the paper  with regressions SE clustered by year

--- 2_analysis/source/analyze_data.r
+++ 2_analysis/source/analyze_data.r
@@ -5,6 +5,8 @@
 library(tidyverse)
 library(stargazer)
 library(ggplot2)
+library(lmtest)
+library(sandwich)
 
 # Paths
 input_dir <- "../../1_data/output"
@@ -15,6 +17,7 @@ output_dir <- "../output"
 main <- function() {
   load("../input/mpg.Rdata")
   regression_table(mpg_clean)
+  regression_table_cluster_year(mpg_clean)
   city_figure(mpg_clean)
   hwy_figure(mpg_clean)
 }
@@ -35,6 +38,42 @@ regression_table <- function(data) {
                                  "Highway fuel economy (mpg)"),
             float = FALSE,
             out = "../output/table_reg.tex")
+}
+
+regression_table_cluster_year <- function(data) {
+
+  reg_cty <- lm(displ ~ cty, data = data)
+  reg_hwy <- lm(displ ~ hwy, data = data)
+  reg_hwy_cty <- lm(displ ~ hwy + cty, data = data)
+
+  vcov_cty <- vcovCL(reg_cty, cluster = ~ year, data = data)
+  vcov_hwy <- vcovCL(reg_hwy, cluster = ~ year, data = data)
+  vcov_hwy_cty <- vcovCL(reg_hwy_cty, cluster = ~ year, data = data)
+
+  se_cty <- sqrt(diag(vcov_cty))
+  se_hwy <- sqrt(diag(vcov_hwy))
+  se_hwy_cty <- sqrt(diag(vcov_hwy_cty))
+
+  p_cty <- coeftest(reg_cty, vcov. = vcov_cty)[, 4]
+  p_hwy <- coeftest(reg_hwy, vcov. = vcov_hwy)[, 4]
+  p_hwy_cty <- coeftest(reg_hwy_cty, vcov. = vcov_hwy_cty)[, 4]
+
+  stargazer(
+    reg_cty,
+    reg_hwy,
+    reg_hwy_cty,
+    align = TRUE,
+    dep.var.labels = c("Engine displacement (L)"),
+    covariate.labels = c(
+      "City fuel economy (mpg)",
+      "Highway fuel economy (mpg)"
+    ),
+    se = list(se_cty, se_hwy, se_hwy_cty),
+    p = list(p_cty, p_hwy, p_hwy_cty),
+    float = FALSE,
+    out = "../output/table_reg_cluster_year.tex"
+  )
+
 }
 
 city_figure <- function(data) {
--- 4_paper/get_inputs.sh
+++ 4_paper/get_inputs.sh
@@ -18,6 +18,7 @@
 INPUT_FILES=(
     ../2_analysis/output/*.jpg 
     ../2_analysis/output/table_reg.tex 
+    ../2_analysis/output/table_reg_cluster_year.tex
     # Add more input paths as needed
 )
 
